52b1de9dd782f172d4f07d2f0b5e258b51fd46a2
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-11-17 08:59:24 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index, :create]
   end
   resources :users, only: [:edit, :update]
 end
