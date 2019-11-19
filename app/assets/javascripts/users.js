27c6a4bd763de9c31bd021c1146db5424eb74031
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-11-19 12:30:57 +0900

    検索時の非同期通信の実装

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,4 +3,10 @@ $(function() {
     let input = $("#user-search-field").val();
     console.log(input);
   });
+  $.ajax({
+    type: "GET",
+    url: '/users',
+    data: { keyword: input },
+    dataType: 'json'
+  })
 });
