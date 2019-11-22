4bf069db2d595f0665d03bcbe397a5c14a5e1a47
kakiharahiroaki <kaki.hiro.88888@gmail.com>
2019-11-22 16:44:29 +0900

    修正実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -9,7 +9,7 @@ $(function(){
                         ${message.user_name}
                       </div>
                       <div class ="upper-message__date">
-                        ${message.created_at}
+                        ${message.time}
                       </div>
                     </div>
                     <div class ="lower-message">
